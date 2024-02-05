b716a51085e955e89dfc7d7a0aafb2711179d033
ejas404 <ijasjaaass004@gmail.com>
2024-02-05 11:13:35 +0530

    hide navbar while scroll

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, Input } from '@angular/core';
+import { Component, DoCheck, HostListener, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,19 +6,36 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent implements DoCheck{
-  @Input()user !:string;
-  isLogged : boolean  = false;
+export class NavbarComponent implements DoCheck {
+  @Input() user !: string;
+  isLogged: boolean = false;
+  visible: boolean = true;
 
-  constructor(private router : Router){}
+  constructor(private router: Router) { }
+  prevScrollpos = window.scrollY;
+
+  ngOnInit(){
+    console.log('previous', this.prevScrollpos)
+  }
+
+  // hide and show the navbar on the socroll event;
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const currentScrollPos = window.scrollY;
+    if (this.prevScrollpos > currentScrollPos) this.visible = true;  
+    else this.visible = false;
+    this.prevScrollpos = currentScrollPos;
+  }
 
   ngDoCheck(): void {
     let currentUrl = this.router.url
 
-    if(currentUrl.includes('profile')){
+    if (currentUrl.includes('profile')) {
       this.isLogged = true
-    }else{
+    } else {
       this.isLogged = false
     }
   }
+
+
 }
