78ebce0e88abc8e8a3c918862031c5d06fc7ceb3
ejas404 <ijasjaaass004@gmail.com>
2024-03-05 14:14:08 +0530

    fix budjet error

--- check.js
+++ check.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCsUQEHma3uNYMzzrj3lX6KEsNce74cj0Q",
-  authDomain: "project-h-6a380.firebaseapp.com",
-  projectId: "project-h-6a380",
-  storageBucket: "project-h-6a380.appspot.com",
-  messagingSenderId: "865488933354",
-  appId: "1:865488933354:web:38f6d75c47aca1a2bfe293",
-  measurementId: "G-YT1CF9WL5T"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCsUQEHma3uNYMzzrj3lX6KEsNce74cj0Q",
+//   authDomain: "project-h-6a380.firebaseapp.com",
+//   projectId: "project-h-6a380",
+//   storageBucket: "project-h-6a380.appspot.com",
+//   messagingSenderId: "865488933354",
+//   appId: "1:865488933354:web:38f6d75c47aca1a2bfe293",
+//   measurementId: "G-YT1CF9WL5T"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
